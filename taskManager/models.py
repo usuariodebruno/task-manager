c6bc6c21b5dd331496c4be797a65fbb456c7f6a3
Bruno Almeida <usuariodebruno@gmail.com>
2024-05-11 18:15:43 -0300

    add: modela dados e registra modelos no admin do projeto

--- taskManager/models.py
+++ taskManager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    
+    def __str__(self):
+        return self.user.username
+
+class Task(models.Model):
+    title = models.CharField(max_length=225, null=False)
+    description = models.CharField(max_length=1225, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
