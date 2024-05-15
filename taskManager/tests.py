d8a358f6d445baddd748f09296457141c7761f5a
Bruno Almeida <usuariodebruno@gmail.com>
2024-05-15 10:37:06 -0300

    test: implementa teste para função deletar tarefa

--- taskManager/tests.py
+++ taskManager/tests.py
@@ -1,20 +1,22 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from django.urls import reverse
+from taskManager.models import Task, Member
 
-from .forms import TaskForm
-from .models import Task, Member
-
-class RegisterTaskViewTest(TestCase):
+class BaseTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('teste_nome', 'teste@email.com', 'senha123')
-        self.member = Member.objects.create(user=self.user)        
-        self.client.login(username='teste_nome', password='senha123')
-
+        self.member = Member.objects.create(user=self.user)
+        self.client = Client()
+        self.client.force_login(self.user)
         self.valid_task_data = {
             'title': 'Tarefa Teste',
             'description': 'Essa é um descrição teste para uma tarefa.'
         }
-        
+        self.task1 = Task.objects.create(title='Tarefa 1', description='Descrição da Tarefa 1', member=self.member)
+        self.task2 = Task.objects.create(title='Tarefa 2', description='Descrição da Tarefa 2', member=self.member)
+
+class RegisterTaskViewTest(BaseTest):
     def test_post_registers_task_with_valid_data(self):
         response = self.client.post('/postRegisterTask/', self.valid_task_data)
 
@@ -25,22 +27,27 @@ class RegisterTaskViewTest(TestCase):
         created_task = Task.objects.get(title=self.valid_task_data['title'], member=self.member)
         self.assertEqual(created_task.description, self.valid_task_data['description'])
 
-class ListTaskViewTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user('teste_nome', 'teste@email.com', 'senha123')
-        self.member = Member.objects.create(user=self.user)
-        self.client = Client()
-        self.client.force_login(self.user)
-        
+class ListTaskViewTest(BaseTest):
     def test_get_list_tasks_view(self):
-        client = Client()  # cliente de teste  
-        client.force_login(self.member.user)  # forçar o login
-
-        task1 = Task.objects.create(title='Tarefa 1', description='Descrição da Tarefa 1', member=self.member)
-        task2 = Task.objects.create(title='Tarefa 2', description='Descrição da Tarefa 2', member=self.member)
-        
-        response = client.get('/listTask/')
+        response = self.client.get('/listTask/')
        
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'taskManager/listTask.html')  
-        self.assertQuerysetEqual(response.context['tasks'], [task1, task2], ordered=False)
+        self.assertQuerysetEqual(response.context['tasks'], [self.task1, self.task2], ordered=False)
+
+class DeleteTaskViewTest(BaseTest):
+    def test_delete_task_view_success(self):
+        response = self.client.post(reverse('taskManager:deleteTask', kwargs={'taskID': self.task1.id}))
+        
+        # checa se a tarefa foi excluida
+        with self.assertRaises(Task.DoesNotExist):
+            Task.objects.get(id=self.task1.id)
+        
+        # checa o redirecionamento
+        self.assertRedirects(response, reverse('taskManager:listTasks'))
+        
+        self.assertEqual(response.status_code, 200)
+        # checa mensagens de sucesso na exclusão
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Tarefa excluida com sucesso!')
